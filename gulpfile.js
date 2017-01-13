aa70966d334bcbe03ea5a9401ceb86c8e13d868f
DevStuffs <bluezerostudios@gmail.com>
2017-01-13 05:39:10 -0500

    added static file watching (add html and json to watch).

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,8 @@ var JsSources = [
     'components/scripts/template.js'
 ]
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json']
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
@@ -50,6 +52,8 @@ gulp.task ('watch', function() {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch(JsSources, ['js']);
     gulp.watch('components/sass/*.scss', ['compass']);
+    gulp.watch(htmlSources, ['html']);
+    gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task ('connect', function() {
@@ -59,5 +63,15 @@ gulp.task ('connect', function() {
     });
 })
 
-gulp.task ('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task ('html', function() {
+    gulp.src(htmlSources)
+    .pipe(connect.reload())
+})
+
+gulp.task ('json', function() {
+    gulp.src(jsonSources)
+    .pipe(connect.reload())
+})
+
+gulp.task ('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
 
