7a2aaff9cf3232bd816e2a6318e131abc91dc5b6
DevStuffs <bluezerostudios@gmail.com>
2017-01-13 05:06:59 -0500

    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -43,4 +43,11 @@ gulp.task('compass', function() {
         .pipe(gulp.dest('builds/development/css'))    
 });
 
+gulp.task ('watch', function() {
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(JsSources, ['js']);
+    gulp.watch('components/sass/*.scss', ['compass']);
+});
+
 gulp.task ('default', ['coffee', 'js', 'compass']);
+
